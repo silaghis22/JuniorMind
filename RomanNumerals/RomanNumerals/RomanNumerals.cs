1694672c0ed9d1d29ddba9a05b4eca47ae715bad
sialghis22 <silaghis22@yahoo.com>
2016-11-15 13:36:36 +0200

    Program simplified

--- RomanNumerals/RomanNumerals/RomanNumerals.cs
+++ RomanNumerals/RomanNumerals/RomanNumerals.cs
@@ -22,76 +22,16 @@ namespace RomanNumerals
         }
         string CreateRomanNumerals(int number)
         {
-            string romanNumerals=string.Empty;
+            string romanNumerals = string.Empty;
             int unit = number % 10;
             int dozens = number / 10;
-            switch (dozens)
-            {
-                case 10:
-                    romanNumerals += "C";
-                    break;
-                case 9:
-                    romanNumerals += "XC";
-                    break;
-                case 8:
-                    romanNumerals += "LXXX";
-                    break;
-                case 7:
-                    romanNumerals += "LXX";
-                    break;
-                case 6:
-                    romanNumerals += "LX";
-                    break;
-                case 5:
-                    romanNumerals += "L";
-                    break;
-                case 4:
-                    romanNumerals += "XL";
-                    break;
-                case 3:
-                    romanNumerals += "XXX";
-                    break;
-                case 2:
-                    romanNumerals += "XX";
-                    break;
-                case 1:
-                    romanNumerals += "X";
-                    break;
-
-
-            }
-            switch (unit)
-            {
-                case 9:
-                    romanNumerals += "IX";
-                    break;
-                case 8:
-                    romanNumerals += "VIII";
-                    break;
-                case 7:
-                    romanNumerals += "VII";
-                    break;
-                case 6:
-                    romanNumerals += "VI";
-                    break;
-                case 5:
-                    romanNumerals += "V";
-                    break;
-                case 4:
-                    romanNumerals += "IV";
-                    break;
-                case 3:
-                    romanNumerals += "III";
-                    break;
-                case 2:
-                    romanNumerals += "II";
-                    break;
-                case 1:
-                    romanNumerals += "I";
-                    break;
-            }
+            string[] arrayDozens = { "X", "XX", "XXX", "XL", "L", "LX", "LXX", "LXXX", "XC", "C" };
+            string[] arrayUnit = { "I", "II", "III", "IV", "V", "VI", "VII", "VIII", "IX" };
+            if (dozens > 0)
+                romanNumerals += arrayDozens[dozens - 1];
+            if (unit > 0)
+                romanNumerals += arrayUnit[unit - 1];
             return romanNumerals;
-
         }
 
     }
