7cfef0f8af82fc7e7eb189621966df3f36e4f749
sialghis22 <silaghis22@yahoo.com>
2016-11-15 15:43:02 +0200

    square one size

---
+++ squares/squares/squares.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace squares
+{
+    [TestClass]
+    public class squares
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(64, NumberTheSquares(1));
+        }
+        int NumberTheSquares(int squares)
+        {
+            int number = 0;
+            if (squares == 1)
+                number = 8 * 8;
+            return number;
+        }
+
+    }
+}
