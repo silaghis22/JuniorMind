bf291b52abcfba02dbb6af53037eb86b75c7f26d
sialghis22 <silaghis22@yahoo.com>
2016-11-15 15:54:04 +0200

    square size four

--- squares/squares/squares.cs
+++ squares/squares/squares.cs
@@ -12,6 +12,7 @@ namespace squares
             Assert.AreEqual(64, NumberTheSquares(1));
             Assert.AreEqual(113, NumberTheSquares(2));
             Assert.AreEqual(149, NumberTheSquares(3));
+            Assert.AreEqual(174, NumberTheSquares(4));
 
         }
         int NumberTheSquares(int squares)
@@ -21,8 +22,10 @@ namespace squares
                 number += 8 * 8;
             if (squares >= 2)
                 number += 7 * 7;
-            if (squares == 3)
+            if (squares >= 3)
                 number += 6 * 6;
+            if (squares == 4)
+                number += 5 * 5;
             return number;
         }
 
