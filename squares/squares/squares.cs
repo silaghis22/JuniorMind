3a00d937d09d91719a0c24b6cde16d4f8b9cc969
sialghis22 <silaghis22@yahoo.com>
2016-11-15 15:48:00 +0200

    square size two

--- squares/squares/squares.cs
+++ squares/squares/squares.cs
@@ -10,12 +10,15 @@ namespace squares
         public void TestMethod1()
         {
             Assert.AreEqual(64, NumberTheSquares(1));
+            Assert.AreEqual(113, NumberTheSquares(2));
         }
         int NumberTheSquares(int squares)
         {
             int number = 0;
-            if (squares == 1)
-                number = 8 * 8;
+            if (squares >= 1)
+                number += 8 * 8;
+            if (squares == 2)
+                number += 7 * 7;
             return number;
         }
 
