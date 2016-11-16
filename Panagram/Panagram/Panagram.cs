79798e10026be04be78111576ec7561c4ff72195
sialghis22 <silaghis22@yahoo.com>
2016-11-16 22:11:44 +0200

    program complete

---
+++ Panagram/Panagram/Panagram.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Panagram
+{
+    [TestClass]
+    public class Panagram
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(false, CheckPanagram( "asdfgv asdas"));
+            Assert.AreEqual(true, CheckPanagram("qw er ty ui op asdfghjkl zxcvb nm"));
+            Assert.AreEqual(true, CheckPanagram("the quick brown fox jumps over the lazy dog"));
+        }
+        bool CheckPanagram(string phrase)
+        {
+            string alphabet =  "qwertyuiopasdfghjklzxcvbnm " ;
+            for (int i = 0; i < alphabet.Length; i++)
+                if (-1 == phrase.IndexOf(alphabet[i]))
+                    return false;
+            return true;
+        }
+    }
+}
