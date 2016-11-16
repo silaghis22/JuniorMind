47ad0abac6e6b57416e09aa858b1dd851c89aae6
sialghis22 <silaghis22@yahoo.com>
2016-11-16 20:54:18 +0200

    I checked first letter

---
+++ Prefix/Prefix/Prefix.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Prefix
+{
+    [TestClass]
+    public class Prefix
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual('a', SearchPrefix("abcd", "acd"));
+            Assert.AreEqual('1', SearchPrefix("acv", "cds"));
+        }
+        char SearchPrefix(string word,string _word)
+        {
+            if(word[0]==_word[0])
+                return word[0];
+            return '1';
+        }
+    }
+}
