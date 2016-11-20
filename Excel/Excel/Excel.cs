5ca69bc2851f8e2e7f5f3c4efdd948fb5864a7a6
sialghis22 <silaghis22@yahoo.com>
2016-11-20 18:11:06 +0200

    number of columns <=26

---
+++ Excel/Excel/Excel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Excel
+{
+    [TestClass]
+    public class Excel
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual('A', TheColumn(1));
+            Assert.AreEqual('Z', TheColumn(26));
+        }
+        char TheColumn(int nrColumn)
+        {
+            string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return alphabet[nrColumn - 1]; 
+        }
+    }
+}
