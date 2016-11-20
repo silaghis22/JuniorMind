d8a93ae219b2c7f49c97e04ddc7f39524d68e49c
sialghis22 <silaghis22@yahoo.com>
2016-11-20 21:44:57 +0200

    number of columns > 26

--- Excel/Excel/Excel.cs
+++ Excel/Excel/Excel.cs
@@ -9,13 +9,47 @@ namespace Excel
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual('A', TheColumn(1));
-            Assert.AreEqual('Z', TheColumn(26));
+            Assert.AreEqual("A", TheColumn(1));
+            Assert.AreEqual("Z", TheColumn(26));
+            Assert.AreEqual("AA", TheColumn(27));
+            Assert.AreEqual("BA", TheColumn(53));
+            Assert.AreEqual("AZ", TheColumn(52));
+            Assert.AreEqual("ZA", TheColumn(677));
+            Assert.AreEqual("ZZ", TheColumn(702));
+            Assert.AreEqual("AAA", TheColumn(703));
+            Assert.AreEqual("ABA", TheColumn(729));
+            Assert.AreEqual("ABC", TheColumn(731));
+            Assert.AreEqual("AZA", TheColumn(1353));
+            Assert.AreEqual("AZZ", TheColumn(1378));
+            Assert.AreEqual("ZZZ", TheColumn(18278));
+            Assert.AreEqual("AAAA", TheColumn(18279));
+            Assert.AreEqual("AAAZ", TheColumn(18304));
+
+
         }
-        char TheColumn(int nrColumn)
+
+
+        string TheColumn(int nrColumn)
         {
+            string nameColumn= string.Empty;
             string alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            return alphabet[nrColumn - 1]; 
+            if (nrColumn < 27)
+                nameColumn = alphabet[nrColumn - 1] + nameColumn;
+            else
+                while (nrColumn>0)
+                {
+                    if (nrColumn % 26 == 0)
+                        nameColumn = alphabet[25] + nameColumn;
+                    else
+                        nameColumn = alphabet[nrColumn % 26 - 1] + nameColumn;
+                    if(nameColumn[0]== 'Z')
+                        nrColumn = (nrColumn / 26)-1;
+                    else
+                        nrColumn = (nrColumn / 26);
+
+                }
+            return nameColumn;
         }
+
     }
 }
