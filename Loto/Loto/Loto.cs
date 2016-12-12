d46f62775939fb7c221385fd27a46b3636964d41
sialghis22 <silaghis22@yahoo.com>
2016-12-13 01:53:10 +0200

    Error !!!

--- Loto/Loto/Loto.cs
+++ Loto/Loto/Loto.cs
@@ -10,18 +10,29 @@ namespace Loto
         public void TestMethod1()
         {
             Assert.AreEqual(1, Combinations(2, 2));
+            Assert.AreEqual(6, Combinations(6, 5));
             Assert.AreEqual(0.0000000, Lottery(1));
+            Assert.AreEqual(0.0000000, Lottery(2));
+            Assert.AreEqual(0, Lottery(3));
         }
         float Lottery(int category)
         {
-            return (float)(Combinations(6, 6) * Combinations(43, 0) / Combinations(49, 6));
+            if(category ==1)
+                return (float)(Combinations(6, 6) * Combinations(43, 0) / Combinations(49, 6));
+            if(category ==2)
+                return (float)((Combinations(6, 5) * Combinations(43, 1) / Combinations(49, 6)));
+            return (float)(Combinations(6, 4) * Combinations(43, 2) / Combinations(49, 6));
+
         }
         static int Combinations(int n,int k)
         {
-            if ((n == k) || (k == 0))
+            if (k == 0)
                 return 1;
             else
-                return (Combinations(n, k - 1) + Combinations(n - 1, k - 1));
+            if (k > n)
+                return 0;
+            else
+                return (Combinations(n - 1, k) + Combinations(n - 1, k - 1));
         }
     }
 }
