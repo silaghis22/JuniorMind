15dbb2bbb98c47cfd6a692a30b9c939ff9ab31ca
sialghis22 <silaghis22@yahoo.com>
2016-12-13 16:32:29 +0200

    error 2 !!!

--- Loto/Loto/Loto.cs
+++ Loto/Loto/Loto.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-
 namespace Loto
 {
     [TestClass]
@@ -11,20 +10,35 @@ namespace Loto
         {
             Assert.AreEqual(1, Combinations(2, 2));
             Assert.AreEqual(6, Combinations(6, 5));
-            Assert.AreEqual(0.0000000, Lottery(1));
-            Assert.AreEqual(0.0000000, Lottery(2));
-            Assert.AreEqual(0, Lottery(3));
         }
-        float Lottery(int category)
+        [TestMethod]
+        public void TestMethod2()
         {
-            if(category ==1)
-                return (float)(Combinations(6, 6) * Combinations(43, 0) / Combinations(49, 6));
-            if(category ==2)
-                return (float)((Combinations(6, 5) * Combinations(43, 1) / Combinations(49, 6)));
-            return (float)(Combinations(6, 4) * Combinations(43, 2) / Combinations(49, 6));
+            Assert.AreEqual("0.00000000", Lottery(1));
+            //Assert.AreEqual(0.00001845, Lottery(2));
+            //Assert.AreEqual(0.00096862, Lottery(3));
 
         }
-        static int Combinations(int n,int k)
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("0.00000151", Lottery5of40(1));
+        }
+        string Lottery5of40(int category)
+        {
+            return (Combinations(5, 5) * Combinations(35, 0) / Combinations(40, 5)).ToString("N8");
+
+        }
+        string Lottery(int category)
+        {
+            if (category == 1)
+                return (Combinations(6, 6) * Combinations(43, 0) / Combinations(49, 6)).ToString("N8");
+            if (category ==2)
+                return (Combinations(6, 5) * Combinations(43, 1) / Combinations(49, 6)).ToString("N8");
+            return (Combinations(6, 4) * Combinations(43, 2) / Combinations(49, 6)).ToString("N8");
+
+        }
+        private static int Combinations(int n, int k)
         {
             if (k == 0)
                 return 1;
