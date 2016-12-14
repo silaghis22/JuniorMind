3dc4288fea6137fa04f4492da87f40cc089f9161
sialghis22 <silaghis22@yahoo.com>
2016-12-14 18:13:45 +0200

    program complete

---
+++ Cub/Cub/Cub.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Cub
+{
+    [TestClass]
+    public class Cub
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(192,Cubes(1));
+            Assert.AreEqual(5942, Cubes(24));
+        }
+        long Cubes (int k)
+        {
+            int count = 0;
+            long number =191;
+            while (count !=k)
+            {
+                number++;
+                if (Pow(number, 3) % 1000 == 888)
+                    count++;
+            }
+            return number;
+        }
+
+        private long Pow(long number, int v)
+        {
+            long pow = 1;
+            while(v>0)
+            {
+                pow *= number;
+                v--;
+            }
+            return pow;
+        }
+    }
+}
