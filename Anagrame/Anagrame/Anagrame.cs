73ed80f705863f2a57063702531554661c2d6f56
sialghis22 <silaghis22@yahoo.com>
2016-12-15 18:57:20 +0200

    program complete

---
+++ Anagrame/Anagrame/Anagrame.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Anagrame
+{
+    [TestClass]
+    public class Anagrame
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual(2, Permutation("ab"));
+            Assert.AreEqual(24, Permutation("sami"));
+        }
+        int Permutation (string word)
+        {
+            int number = 1;
+            for (int i = 1; i <= word.Length; i++)
+                number *= i;
+            return number;
+        }
+    }
+}
