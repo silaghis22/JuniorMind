3c675ac21753e3648f5cd2bbb5cf5dd0de3fe555
sialghis22 <silaghis22@yahoo.com>
2016-12-16 20:56:31 +0200

    I added the variable "repetition" which counts the rehearsals

--- Anagrame/Anagrame/Anagrame.cs
+++ Anagrame/Anagrame/Anagrame.cs
@@ -9,13 +9,19 @@ namespace Anagrame
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual(2, Permutation("ab"));
+            Assert.AreEqual(2, Permutation( "ab"));
             Assert.AreEqual(24, Permutation("sami"));
+            Assert.AreEqual(2, Permutation("abb"));
         }
         int Permutation (string word)
         {
+            int repetition = 0;
+            for (int i = 0; i < word.Length; i++)
+                for (int j = i + 1; j < word.Length; j++)
+                    if (word[i] == word[j])
+                        repetition++;
             int number = 1;
-            for (int i = 1; i <= word.Length; i++)
+            for (int i = 1; i <= word.Length-repetition; i++)
                 number *= i;
             return number;
         }
