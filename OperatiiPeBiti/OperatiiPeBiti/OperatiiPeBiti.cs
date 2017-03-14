6d451a321272102dbcf74893f24b48d6cb56beef
sialghis22 <silaghis22@yahoo.com>
2017-03-14 20:35:18 +0200

    add shift left and shift right

--- OperatiiPeBiti/OperatiiPeBiti/OperatiiPeBiti.cs
+++ OperatiiPeBiti/OperatiiPeBiti/OperatiiPeBiti.cs
@@ -11,6 +11,8 @@ namespace OperatiiPeBiti
         {
             CollectionAssert.AreEqual(new byte[] { 1, 0, 0, 0, 1, 1 },ConversionFromDecimalInRandomBase(49,2));
             Assert.AreEqual(49, ConversionFromRandomBaseInDecimal(new byte[] { 1, 0, 0, 0, 1, 1 }, 2));
+            Assert.AreEqual(1, ShiftRight(new byte[] {0,0,0,1 }, 2, 3));
+            Assert.AreEqual(20, ShiftLeft(new byte[] { 1, 0, 1 }, 2, 2));
         }
         byte[] ConversionFromDecimalInRandomBase (int number , int randomBase)
         {
@@ -39,6 +41,20 @@ namespace OperatiiPeBiti
             int number = 0;
             for (int i = 0; i < array.Length; i++)
                 number +=array[i]*(int)( Math.Pow(randomBase, i)); 
+            return number;
+        }
+        int ShiftRight(byte[] array, int randomBase, int numberOfPositions)
+        {
+            int number = ConversionFromRandomBaseInDecimal(array, randomBase);
+            for (int i = 1; i <= numberOfPositions; i++)
+                number /= randomBase;
+            return number;
+        }
+        int ShiftLeft(byte[] array, int randomBase, int numberOfPositions)
+        {
+            int number = ConversionFromRandomBaseInDecimal(array, randomBase);
+            for (int i = 1; i <= numberOfPositions; i++)
+                number *= randomBase;
             return number;
         }
 
